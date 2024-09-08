d971fb9ff783754d0ba43b16be53dd2010a41c40
Abdulkarimov Oleg <olegabdulkarimov26@gmail.com>
2024-09-08 15:23:34 +0300

    Изучение JavaScript

---
+++ script.js
@@ -0,0 +1,25 @@
+alert("Я JavaScript!");
+
+let message;
+message = "Здравствуй пользователь";
+alert(message);
+
+let message1 = "Я помогу тебе в работе с кодом";
+alert(message1)
+
+let message2 = "Но сначало...",
+    message3 = "Изучи руководсто";
+alert(message2);
+alert(message3);
+
+const message4 = "Пользователь зарегистрирован 08.09.2024";
+alert(message4);
+
+let message5 = "фокусировка";
+alert(`Полная ${message5} на изучении JavaScript`);
+
+let name = prompt("Пользователь, введите ваше имя");
+alert(`Полученны новые данные, имя пользователя ${name}`);
+
+let work = confirm("Домашняя робата завершенна?");
+alert(work);
